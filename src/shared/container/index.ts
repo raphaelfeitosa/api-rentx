3778fd85302326839120a2211d0dcb1cae58359b
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-06 10:30:46 -0300

    Create migration, repository, controller and usercase User

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,10 @@ import { CategoriesRepository } from "../../modules/cars/repositories/Implementa
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/Implementations/SpecificationsRepository";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUserRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UserRepository";
+
+
 
 //const containerRegister = (): void => {
 container.registerSingleton<ICategoriesRepository>(
@@ -17,6 +21,11 @@ container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
 );
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
+)
 //}
 
 //export { containerRegister };
