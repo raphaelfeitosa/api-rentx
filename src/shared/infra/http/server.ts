142416b68c54d428098705387e6e31017ac18962
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-21 12:17:57 -0300

    create seed admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,7 +4,7 @@ import 'express-async-errors';
 
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm"
+import createConnection from "@shared/infra/typeorm"
 import "@shared/container"
 
 
@@ -12,6 +12,7 @@ import swaggerFile from "../../../swagger.json";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
