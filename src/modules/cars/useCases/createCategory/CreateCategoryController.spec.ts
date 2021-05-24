0669063577b1d9cf30b56c79fcbba509b1b8291b
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-24 19:25:58 -0300

    create test post a new category http status 400

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -52,5 +52,27 @@ describe("should be able to create a new Category", () => {
             });
 
         expect(response.status).toBe(201);
-    })
+    });
+
+    it("should not be able to create a new category with name exists", async () => {
+        const responseToken = await request(app).post("/sessions")
+            .send({
+                email: "admin@rentx.com.br",
+                password: "admin"
+            });
+
+        const { token } = responseToken.body
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Supertest",
+                description: "Category Supertest"
+            })
+            .set({
+                Authorization: `Bearer ${token}`
+            });
+
+        expect(response.status).toBe(400);
+    });
 });
